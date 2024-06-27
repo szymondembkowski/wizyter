12ba56af1325a12103f6e4c7970299bedbea9fb4
Szymon <dembkowski123@gmail.com>
2024-06-27 14:12:29 +0200

    Clear start

---
+++ wizyter/src/main/java/com/szymon/wizyter/WizyterApplication.java
@@ -0,0 +1,13 @@
+package com.szymon.wizyter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WizyterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WizyterApplication.class, args);
+	}
+
+}
---
+++ wizyter/src/test/java/com/szymon/wizyter/WizyterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szymon.wizyter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizyterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
